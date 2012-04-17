900e48fe5bf184838d2cd1b9fe02659c3b712944
Yohei YASUKAWA <yasulab@gmail.com>
2012-04-16 18:35:31 -0700

    Reverse to init.

--- app/models/command.rb
+++
@@ -1,6 +0,0 @@
-class Command < ActiveRecord::Base
-  attr_accessible :command, :description
-  
-  validates :command, presence: true
-  validates :description, presence: true, length: { maximum: 140 }
-end
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-EmacsBot::Application.config.secret_token = '5276c028c0790d0b9d1db0d9b41935f5a18304c5cbad0965bce57bb8dbe0ca5e3865efbc28cbe09eb3bfe841071cb122f2921f3e7423a9084d5d3efa179d1291'
